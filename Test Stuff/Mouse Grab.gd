84d850e5eb2412af6336d07ff02dd0e2cf77e2f3
Matthew Buckman <buckmanm@colostate.edu>
2025-04-08 10:36:48 -0600

    mouse wheel movement & grabbable sphere scene

--- Test Stuff/Mouse Grab.gd
+++ Test Stuff/Mouse Grab.gd
@@ -3,19 +3,27 @@ extends MeshInstance3D
 var mouseOver:bool = false
 var holding:bool = false
 var offset:Vector3
+@onready var dist:float = get_viewport().get_camera_3d().position.distance_to(position)
 
 func _process(delta: float) -> void:
 	var mousePos = get_viewport().get_mouse_position()
 	if mouseOver or holding:
-		
+		if Input.is_action_just_released("Zoom_Out"):
+			dist -= .1
+			print(dist)
+		if Input.is_action_just_released("Zoom_In"):
+			dist += .1
+			print(dist)
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			offset = get_viewport().get_camera_3d().project_position(mousePos, get_viewport().get_camera_3d().project_ray_origin(mousePos).z)
+			offset = get_viewport().get_camera_3d().project_position(mousePos, dist) #* (get_viewport().get_camera_3d().position.distance_to(position) * get_viewport().get_camera_3d().position.direction_to(position))
 			position = offset
+			print(offset)
 			print(get_viewport().get_camera_3d().project_ray_origin(mousePos).z)
 			holding = true
 		else:
 			holding = false
-	print(getMouseWorldPos())
+	else:
+		dist = get_viewport().get_camera_3d().position.distance_to(position)
 
 func _on_area_3d_mouse_entered() -> void:
 	mouseOver = true;
@@ -28,18 +36,3 @@ func _on_area_3d_mouse_exited() -> void:
 	scale = scale - Vector3(.05, .05, .05)
 	print("end hover")
 	pass # Replace with function body.
-	
-	
-# from: https://github.com/Chevifier/ChevifierTutorials
-# usefull if you don't wnat to use 
-func getMouseWorldPos():
-	var space = get_world_3d().direct_space_state
-	var mousePos = get_viewport().get_mouse_position()
-	var start = get_viewport().get_camera_3d().project_ray_origin(mousePos)
-	var end = get_viewport().get_camera_3d().project_position(mousePos, 10)
-	var rayParams = PhysicsRayQueryParameters3D.new()
-	rayParams.from = start
-	rayParams.to = end
-	
-	var intersect = space.intersect_ray(rayParams)
-	return intersect
