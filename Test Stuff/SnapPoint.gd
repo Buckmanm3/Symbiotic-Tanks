9b7f2f2f8bb8089393dd8fc60a30e8a084dc882b
Matthew Buckman <buckmanm@colostate.edu>
2025-04-10 15:03:00 -0600

    Added snapping to snap points

--- Test Stuff/SnapPoint.gd
+++ Test Stuff/SnapPoint.gd
@@ -1,15 +1,16 @@
 class_name SnapPoint extends MeshInstance3D
 
-
-
-
-
-
+var snapped : bool = false
+var connectedNode : Grabbable = null
 
 func _on_area_3d_area_shape_entered(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
+	
 	if area.get_parent() != null:
 		if area.get_parent().has_method("getGrabed"):
-			print("SNAP")
+			if connectedNode == null:
+				snapped = true
+				connectedNode = area.get_parent()
+				print("Snap")
 			# maybe pass this object to Grab sphere so it can check if it should snap or not
 			# clear node when node exits area
 			# have bool to detect if node has already snapped
@@ -17,4 +18,15 @@ func _on_area_3d_area_shape_entered(area_rid: RID, area: Area3D, area_shape_inde
 
 
 func _on_area_3d_area_shape_exited(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
+	if area.get_parent() != null:
+		if area.get_parent().has_method("getGrabed"):
+			if connectedNode != null:
+				snapped = false
+				connectedNode = null
+				print("Unsnap")
 	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	if snapped:
+		if !connectedNode.holding:
+			connectedNode.global_position = global_position
