c8bc2e53cda640fbae64c73681953572b02ce0c4
Matthew Buckman <buckmanm@colostate.edu>
2025-04-10 14:51:51 -0600

    fixed dist on grab sphere

--- Test Stuff/Mouse Grab.gd
+++ Test Stuff/Mouse Grab.gd
@@ -1,53 +1,45 @@
 class_name Grabbable extends MeshInstance3D
 
-"""
-Results:
-	-proof of concept is great, it showed this project is a realistic goal/idea
-	-having the code within the object is nice for simplicty sake but can be an issue since its isolated
-		-Bug: player can grab multiple spheres at once when already holding
-	-area 3d signals need to be local so there needs to be some code within the object
-	-need ref to player object which can tell if an object is currently being held
-	-Snap Grid stuff is very possible it just may need some good structure planning
-		-if a both snap-node and node need to store if they are attached
-			-avoid snapping multiple nodes to one grid spot
-			-node needs position to update each frame
-			-node needs to know when it is released within deadzone so it can snap
-"""
-
-
 var mouseOver:bool = false
 var holding:bool = false
 var offset:Vector3
-@onready var area : Area3D = $Area3D
-@onready var dist : float = get_viewport().get_camera_3d().position.distance_to(area.position)
-@onready var player : Player = get_tree().root.get_child(0).get_node("Player")
+var area : Area3D  
+var player : Player  
+var dist : float 
+
+func _ready() -> void:
+	area = $Area3D
+	player = get_tree().root.get_child(0).get_node("Player")
+	dist = global_position.distance_to(get_viewport().get_camera_3d().global_position)
 
 func _process(delta: float) -> void:
 	var mousePos = get_viewport().get_mouse_position()
 	if (mouseOver or holding) && !player.movingCamera:
-		if Input.is_action_just_released("Zoom_Out"):
-			dist -= .1
-		if Input.is_action_just_released("Zoom_In"):
-			dist += .1
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			offset = get_viewport().get_camera_3d().project_position(mousePos, dist)
-			position = offset
-			holding = true
-			player.holding = true
-		else:
-			holding = false
-			player.holding = false
-	else:
-		dist = get_viewport().get_camera_3d().position.distance_to(area.position)
+		if !((player.holding || holding) && !(player.holding and holding)): # not xor :/
+			if Input.is_action_just_released("Zoom_Out"):
+				dist -= .1
+			if Input.is_action_just_released("Zoom_In"):
+				dist += .1
+			if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+				offset = get_viewport().get_camera_3d().project_position(mousePos, dist)
+				position = offset
+				holding = true
+				player.holding = true
+			else:
+				holding = false
+				player.holding = false
 	
-
 func _on_area_3d_mouse_entered() -> void:
-	mouseOver = true;
-	scale = scale + Vector3(.05, .05, .05)
+	if !((player.holding || holding) && !(player.holding and holding)):
+		mouseOver = true;
+		scale = scale + Vector3(.05, .05, .05)
 
 func _on_area_3d_mouse_exited() -> void:
-	mouseOver = false;
-	scale = scale - Vector3(.05, .05, .05)
+	if !((player.holding || holding) && !(player.holding and holding)):
+		mouseOver = false;
+		scale = scale - Vector3(.05, .05, .05)
+		if !holding:
+			dist = global_position.distance_to(get_viewport().get_camera_3d().global_position)
 	
 func getGrabed():
 	return holding
