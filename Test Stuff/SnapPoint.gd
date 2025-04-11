24c073275ce9025a9c4ac8f46578b934481b4c6b
Matthew Buckman <buckmanm@colostate.edu>
2025-04-10 23:28:18 -0600

    small tweaks and added sample vehicle

--- Test Stuff/SnapPoint.gd
+++ Test Stuff/SnapPoint.gd
@@ -10,12 +10,6 @@ func _on_area_3d_area_shape_entered(area_rid: RID, area: Area3D, area_shape_inde
 			if connectedNode == null:
 				snapped = true
 				connectedNode = area.get_parent()
-				print("Snap")
-			# maybe pass this object to Grab sphere so it can check if it should snap or not
-			# clear node when node exits area
-			# have bool to detect if node has already snapped
-	pass # Replace with function body.
-
 
 func _on_area_3d_area_shape_exited(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
 	if area.get_parent() != null:
@@ -23,8 +17,6 @@ func _on_area_3d_area_shape_exited(area_rid: RID, area: Area3D, area_shape_index
 			if connectedNode != null:
 				snapped = false
 				connectedNode = null
-				print("Unsnap")
-	pass # Replace with function body.
 
 func _process(delta: float) -> void:
 	if snapped:
