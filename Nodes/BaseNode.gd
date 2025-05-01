499cfd32ed0109bfebe9a55ec4cfe1b6069ad187
Matthew Buckman <buckmanm@colostate.edu>
2025-05-01 15:20:04 -0600

    added friction

--- Nodes/BaseNode.gd
+++ Nodes/BaseNode.gd
@@ -4,6 +4,7 @@ class_name BaseNode
 var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 var jumpTime: float = 1
 var jumpCounter: float = 0
+@export var friction: float = .1
 
 var mouseOver:bool = false
 var holding:bool = false
@@ -16,9 +17,9 @@ var snapped: bool = false
 @onready var base : RigidBody3D = $"."
 @onready var highlight : MeshInstance3D = $Highlight
 
-@export var moveBonus: float = 0
-@export var health: float = 0
-@export var turnBonus: float = 0
+@export var moveBonus: float = 0.1
+@export var health: float = 0.1
+@export var turnBonus: float = 0.1
 
 func _ready() -> void:
 	player = get_tree().root.get_child(0).get_node("Player")
@@ -43,15 +44,14 @@ func _process(delta: float) -> void:
 				player.holding = false
 	if (!snapped && !holding):
 		if (jumpCounter >= jumpTime):
-			var impulse = Vector3(rng.randf_range(-0.1,0.1), rng.randf_range(0.0, .5), rng.randf_range(-0.,0.1))
+			var impulse = Vector3(rng.randf_range(-0.3,0.3), rng.randf_range(0.0, 1), rng.randf_range(-0.3,0.3))
 			base.apply_impulse(impulse, position)
 			jumpCounter = 0
-			jumpTime = rng.randf_range(5.0, 10.0)
+			jumpTime = rng.randf_range(3.0, 5.0)
 			print("JUMP!")
 		else:
 			jumpCounter += delta
-			
-			
+			applyFriction()	
 		
 
 func getGrabed():
@@ -68,3 +68,8 @@ func _on_area_3d_mouse_exited() -> void:
 		#scale = scale - Vector3(.05, .05, .05)
 		if !holding:
 			dist = global_position.distance_to(get_viewport().get_camera_3d().global_position)
+
+func applyFriction():
+	var linearVel = Vector3(lerp(base.linear_velocity.x, 0.0, friction), base.linear_velocity.y-.1, lerp(base.linear_velocity.z, 0.0, friction))
+	base.linear_velocity = linearVel
+	base.angular_velocity = lerp(base.angular_velocity, Vector3.ZERO, friction)
