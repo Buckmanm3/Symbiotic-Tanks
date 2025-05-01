0d11b62febaeb18252b2b275ed22b0dea7c27096
Matthew Buckman <buckmanm@colostate.edu>
2025-05-01 16:04:30 -0600

    added animation to main menu

--- Menus/Main_Menu.gd
+++ Menus/Main_Menu.gd
@@ -1,9 +1,14 @@
 extends Control
 
+func _ready() -> void:
+	$Panel/AnimatedSprite2D.play()
+
+func _process(delta: float) -> void:
+	$Panel/AnimatedSprite2D.scale = $Panel.scale
+	$Panel/AnimatedSprite2D.position = $Panel.size/2
+
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Test Stuff/Grab Sphere.tscn")
-	
-
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Menus/Options_Menu.tscn")
