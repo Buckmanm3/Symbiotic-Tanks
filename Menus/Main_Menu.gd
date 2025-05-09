927ae126baf501fb4544130c28f9f1bc9555bb94
Matthew Buckman <buckmanm@colostate.edu>
2025-05-09 17:58:31 -0600

    main menu junk

--- Menus/Main_Menu.gd
+++ Menus/Main_Menu.gd
@@ -1,19 +1,18 @@
-extends Control
+extends CanvasLayer
 
 func _ready() -> void:
-	$Panel/AnimatedSprite2D.play()
+	pass
 
-func _process(delta: float) -> void:
-	$Panel/AnimatedSprite2D.scale = $Panel.scale
-	$Panel/AnimatedSprite2D.position = $Panel.size/2
+
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Test Stuff/Grab Sphere.tscn")
+	pass # Replace with function body.
+
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Menus/Options_Menu.tscn")
 	pass # Replace with function body.
 
-
-func _on_exit_pressed() -> void:
+func _on_quit_pressed() -> void:
 	get_tree().quit()
