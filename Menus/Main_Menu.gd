0157ad4690ccbacbe19581c31c2bb8b37ea043f0
Matthew Buckman <buckmanm@colostate.edu>
2025-05-10 14:00:24 -0600

    Add Video to main menu and new options placeholder

--- Menus/Main_Menu.gd
+++ Menus/Main_Menu.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
+# @onready var videoPlayer : VideoStreamPlayer = $Control/AspectRatioContainer/VideoStreamPlayer
+
+
 
 func _ready() -> void:
+	# videoPlayer.expand = true;
 	pass
 
 
@@ -11,7 +15,7 @@ func _on_play_pressed() -> void:
 
 
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://Menus/Options_Menu.tscn")
+	get_tree().change_scene_to_file("res://Menus/NewOptions.tscn")
 	pass # Replace with function body.
 
 func _on_quit_pressed() -> void:
