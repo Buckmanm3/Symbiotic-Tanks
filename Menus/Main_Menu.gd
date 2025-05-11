87e7d2fea9016e0b9bd484e6a77ad5c761c90940
Matthew Buckman <buckmanm@colostate.edu>
2025-05-11 17:11:27 -0600

    Level Select and Level1 placeholder

--- Menus/Main_Menu.gd
+++ Menus/Main_Menu.gd
@@ -10,12 +10,7 @@ func _ready() -> void:
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Test Stuff/Grab Sphere.tscn")
-	pass # Replace with function body.
-
-
-func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://Menus/NewOptions.tscn")
+	get_tree().change_scene_to_file("res://Menus/LevelSelect.tscn")
 	pass # Replace with function body.
 
 func _on_quit_pressed() -> void:
---
+++ Menus/level_select.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+
+func _on_lv_1_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_lv_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_lv_3_pressed() -> void:
+	pass # Replace with function body.
