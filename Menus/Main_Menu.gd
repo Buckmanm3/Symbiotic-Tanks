48113e46b42823bc157334707e23573e5aa6b295
Matthew Buckman <buckmanm@colostate.edu>
2025-05-13 17:00:54 -0600

    Final Bits of prototype

--- Menus/Main_Menu.gd
+++ Menus/Main_Menu.gd
@@ -2,16 +2,16 @@ extends CanvasLayer
 # @onready var videoPlayer : VideoStreamPlayer = $Control/AspectRatioContainer/VideoStreamPlayer
 
 
-
 func _ready() -> void:
 	# videoPlayer.expand = true;
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	pass
-
 
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Menus/LevelSelect.tscn")
 	pass # Replace with function body.
+
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
