7ae17f9be3580ffdb0babb1ae842b46164d79335
Matthew Buckman <buckmanm@colostate.edu>
2025-05-13 17:02:19 -0600

    remove quit button

--- Menus/Main_Menu.gd
+++ Menus/Main_Menu.gd
@@ -13,10 +13,6 @@ func _on_play_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
-
-
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Menus/NewOptions.tscn")
 	pass # Replace with function body.
